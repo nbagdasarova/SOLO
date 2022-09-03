55542e3ffeb2e37c0a3f2fca0b225bd400998ed2
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-09-03 18:58:54 +0400

    Update common.js

--- js/common/common.js
+++ js/common/common.js
@@ -1,1 +1,1 @@
-export const URL='https://solo.ge/api/developers/items/common/608aaad7ae3b47ff23daa433?skip=0&limit=9';
+export const soloUrl='https://solo.ge/api/developers/items/common/608aaad7ae3b47ff23daa433?skip=0&limit=9';
