36a61f2870744fc5cfa3790c88a0dc1efcf297e2
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-09-03 19:04:01 +0400

    Add files via upload

--- js/module/catalog.js
+++ js/module/catalog.js
@@ -1,1 +1,33 @@
+export class _Catalog {
+  constructor(appJs) {
+    this.appJs = appJs;
+  }
 
+  fetchCatalog() {
+    const catalog = this.appJs
+      .map((elem) => {
+        return `<div class="developer-item">
+                <div class="developer-img" style="background-image: url('${elem.image.url}');"></div>
+                <div class="developer-title">
+                    <p>${elem.projectName}</p>
+                </div>
+                <div class="developer-subtitle">
+                    <p>${elem.priceLabel}</p>
+                    <span>${elem.address}</span>
+                </div>
+                <div class="link-btn">
+                    <p>გაიგეთ მეტი</p>
+                </div>
+            </div>`;
+      })
+      .join("");
+    console.log("catalog", catalog);
+    document
+      .querySelector("#catalogId")
+      .insertAdjacentHTML("afterbegin", catalog);
+  }
+
+  render() {
+    this.fetchCatalog();
+  }
+}
