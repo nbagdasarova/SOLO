6e2565ad39c25d8d6b183aad568a2421647ef1df
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-09-05 21:56:38 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,22 +22,13 @@ fetchCatalog(soloUrl);
 
 // if city checkboxed
 
-// function getCityUrl(value) {
-//   let filteredUrl = soloUrl + `&cityParam=${value}`;
-//   return filteredUrl;
-// }
-
-// fetchCatalog (getCityUrl('ბათუმი'))
-
-function getUrl() {
+function cityFilter() {
   let checkedCity = document.getElementsByClassName("checkbox");
   let newUrl = soloUrl + "&cityParam=";
-  let checkedRadio = document.getElementsByClassName("radio");
   for (let i = 0; i < checkedCity.length; i++) {
     let elem = checkedCity[i];
     elem.addEventListener("change", () => {
       if (event.currentTarget.checked) {
-        cityArr.push({ id: i, value: elem.value });
         newUrl = newUrl + `${elem.value}` + ",";
       } else newUrl = soloUrl;
 
@@ -46,52 +37,9 @@ function getUrl() {
         .then((data) => {
           let res = data.data.items;
           let ctl = new _Catalog(res);
-          console.log("test", res);
           ctl.render();
         });
     });
-
-    // for (let i = 0; i < checkedRadio.length; i++) {
-    //     let item = checkedRadio[i];
-    //     item.addEventListener("click", () => {
-    //       if (event.currentTarget.checked){
-    //       newUrl =  newUrl + `&fromParam=${minValue}`
-    //     } });
-
-    //     }
-    //   }
   }
-
-  console.log(newUrl);
 }
-
-getUrl();
-
-// for (let i = 0; i < checkedCity.length; i++) {
-
-//   let elem = checkedCity[i];
-//   elem.addEventListener("change", () => {
-//        if (event.currentTarget.checked) {
-//             let newUrl = soloUrl + `&cityParam=${elem.value}`;
-//       fetchCatalog(newUrl);
-//     } else {
-//       fetchCatalog(soloUrl);
-//     }
-//   });
-// }
-// // if radio btn ia clicked
-
-// function getPriceUrl(minValue, maxValue) {
-//   let filteredUrl = soloUrl + `&fromParam=${minValue}`;
-//   return filteredUrl;
-// }
-
-// // let checkedRadio = document.getElementsByClassName("radio");
-// for (let i = 0; i < checkedRadio.length; i++) {
-//   let elem = checkedRadio[i];
-//   elem.addEventListener("click", () => {
-//     fetchCatalog(getPriceUrl(elem.value));
-//   });
-// }
-
-// fetchCatalog(soloUrl);
+cityFilter();
