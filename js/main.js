a1cb5aadb014baa4bbef7f243d84785fdd22c086
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-09-05 14:02:28 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import { _Catalog } from "./module/catalog.js";
 import { soloUrl } from "./common/common.js";
+import { _Home } from "./module/home.js";
+import { app } from "./common/common.js";
+
+let homePage = new _Home(app);
+
+let list = homePage.render();
 
 function fetchCatalog(urlValue) {
   let url = urlValue;
@@ -12,11 +18,12 @@ function fetchCatalog(urlValue) {
     });
 }
 
-// if city checkboxed 
- 
-function getCityUrl(value) {   
- let filteredUrl=soloUrl+`&cityParam=${value}`;
-return filteredUrl}
+// if city checkboxed
+
+function getCityUrl(value) {
+  let filteredUrl = soloUrl + `&cityParam=${value}`;
+  return filteredUrl;
+}
 
 // fetchCatalog (getCityUrl('ბათუმი'))
 
@@ -24,25 +31,26 @@ let checked = document.getElementsByClassName("checkbox");
 for (let i = 0; i < checked.length; i++) {
   let elem = checked[i];
   elem.addEventListener("change", () => {
-    if (event.currentTarget.checked) {        
-    fetchCatalog(getCityUrl (elem.value));}
-    else {fetchCatalog(soloUrl)}
+    if (event.currentTarget.checked) {
+      fetchCatalog(getCityUrl(elem.value));
+    } else {
+      fetchCatalog(soloUrl);
+    }
   });
 }
 // if radio btn ia clicked
 
-function getPriceUrl(minValue,maxValue) {
-    let filteredUrl=soloUrl+`&fromParam=${minValue}`;
-    return filteredUrl}
+function getPriceUrl(minValue, maxValue) {
+  let filteredUrl = soloUrl + `&fromParam=${minValue}`;
+  return filteredUrl;
+}
 
-    let checkedRadio = document.getElementsByClassName("radio");
-    for (let i = 0; i < checkedRadio.length; i++) {
-      let elem = checkedRadio[i];
-      elem.addEventListener("click", () => {
-        i   
-        fetchCatalog(getPriceUrl(elem.value));})
-        
-      
-      }
+let checkedRadio = document.getElementsByClassName("radio");
+for (let i = 0; i < checkedRadio.length; i++) {
+  let elem = checkedRadio[i];
+  elem.addEventListener("click", () => {
+    fetchCatalog(getPriceUrl(elem.value));
+  });
+}
 
 fetchCatalog(soloUrl);
